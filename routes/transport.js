77530da83f258933be37a54f2f96c5854af31dc6
Tim <timd1026@gmail.com>
2019-01-03 15:50:38 -0700

    added-env

--- routes/transport.js
+++ routes/transport.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const transportRouter = express.Router() 
 const nodemailer = require('nodemailer')
-const creds = require('../config')
+
 
 const transport = {
     host: 'smtp.gmail.com',
     auth: {
-        user: creds.USER,
-        pass: creds.PASS
+        user: process.env.USER,
+        pass: process.env.PASS
     }
 }
 
