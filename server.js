facc5698093d9a94e7d27bd152757e5677c8f22e
Tim <timd1026@gmail.com>
2019-01-03 13:59:39 -0700

    added-express-static

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5678
+const path = require("path")
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use('/forwarder', require('./routes/forwarders'))
 app.use('/comment', require('./routes/comments'))
 app.use('/transport', require('./routes/transport'))
+
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/1stfullstack', {useNewUrlParser: true}, () => {
     console.log('Booted up!')
