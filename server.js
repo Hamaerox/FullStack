57ac0cd80cac119a01648f16b146085e20debbc9
Tim <timd1026@gmail.com>
2019-01-03 13:53:00 -0700

    build-updates

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ app.use('/forwarder', require('./routes/forwarders'))
 app.use('/comment', require('./routes/comments'))
 app.use('/transport', require('./routes/transport'))
 
-mongoose.connect('mongodb://localhost:27017/1stfullstack', {useNewUrlParser: true}, () => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/1stfullstack', {useNewUrlParser: true}, () => {
     console.log('Booted up!')
 })
 
 app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
 
 app.listen(PORT, () => {
